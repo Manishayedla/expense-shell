5922ba7a1cf92f1cb5ca983bc9cd4ab43f59ccaa
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-15 18:42:40 -0500

    run

--- mysql.sh
+++ mysql.sh
@@ -42,4 +42,5 @@ VALIDATE $? "Starting MySQL Server"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "Setting up root password"
 
-echo "hello1"
+echo "hello12"
+echo "hello"
