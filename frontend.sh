11689b9503be77e5ff76f2d2a823229eb723ce05
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-16 17:57:32 -0500

    run

--- frontend.sh
+++ frontend.sh
@@ -33,19 +33,26 @@ fi
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "Enabling nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/*
 VALIDATE $? "removing the existing content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading frontend code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
+
+#check your repo and path
+cp /home/ec2-user/expense-shell/expense.conf /etc/systemd/system/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense path"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
